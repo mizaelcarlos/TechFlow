23ce98d55a489998ecec241781a50dbbb756d2b9
andree060 <andreluyz2001@gmail.com>
2025-03-18 11:23:38 -0300

    alteraçao na migrate de users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
             $table->string('email')->unique();
+            $table->string('telefone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- schema.sql
+++ schema.sql
@@ -3,24 +3,15 @@ create database techflow;
 use database techflow;
 
 create table perfil(
-    id int primary key,
+    id bigint  primary key AUTO_INCREMENT,
     nome varchar(100) not null
 );
 
 
-create table usuario(
-    id int primary key,
-    nome varchar(100) not null,
-    telefone varchar(14) not null,
-    email varchar(100),
-    senha varchar(100),
-    perfil_id int not null,
-    foreign key (perfil_id) references perfil(id)
-);
 
 
 create table chamado(
-    id int primary key,
+    id int primary key AUTO_INCREMENT,
     titulo varchar(100) not null,
     descricao varchar(45) not null,
     usuario_solicitante_id int not null,
@@ -28,40 +19,43 @@ create table chamado(
     risco_id int not null,
     status_id int not null,
     setor_id int not null,
-    foreign key (usuario_solicitante_id) references usuario(id),
-    foreign key (usuario_atendente_id) references usuario(id),
+    foreign key (usuario_solicitante_id) references users(id),
+    foreign key (usuario_atendente_id) references users(id),
     foreign key (risco_id) references risco(id),
     foreign key (status_id) references status(id),
     foreign key (setor_id) references setor(id)
 );
 
 create table status(
-    id int primary key,
+    id int primary key AUTO_INCREMENT,
     nome varchar(100) not null,
     cor varchar(100) null
 );
 
 create table risco(
-    id int primary key,
+    id int primary key AUTO_INCREMENT,
     nome varchar(100) not null,
     cor varchar(100) not null
 );
 
 create table midia(
-    id int primary key,
+    id int primary key AUTO_INCREMENT,
     midia varchar(100) not null
 );
 
 create table setor(
-    id int primary key,
+    id int primary key AUTO_INCREMENT,
     nome varchar(45) not null
 );
 
 create table chamado_midia(
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     chamado_id int not null,
     midia_id int not null,
     foreign key (chamado_id) references chamado(id),
     foreign key (midia_id) references midia (id)
 );
 
+
+ALTER TABLE users ADD perfil_id bigint unsigned;
+ALTER TABLE users ADD CONSTRAINT fk_peril_id FOREIGN KEY (perfil_id) REFERENCES perfil (id);
