de3c28861fdcc502d14fdf5fed5972b8391f603a
Vanusa <vanusa.psenai@gmail.com>
2025-03-24 11:24:27 -0300

    inicio do crud de status

--- app/Models/Chamado.php
+++ app/Models/Chamado.php
@@ -9,7 +9,7 @@ class Chamado extends Model
 {
     use HasFactory;
 
-    protected $table = 'chamados';  
+    protected $table = 'chamado';  
     // Campos que podem ser preenchidos em massa
     protected $fillable = [
         'titulo',
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StatusController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('status',[StatusController::class,'index'])->name('status.index');
+Route::get('/status/create',[StatusController::class,'create'])->name('status.create');
