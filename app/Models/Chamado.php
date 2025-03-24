0b3ba1f853e819ad534b0ba6a72524c0971e8dd5
Vanusa <vanusa.psenai@gmail.com>
2025-03-24 08:50:55 -0300

    criando model chamado

---
+++ app/Models/Chamado.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chamado extends Model
+{
+    use HasFactory;
+
+    protected $table = 'chamados';  
+    // Campos que podem ser preenchidos em massa
+    protected $fillable = [
+        'titulo',
+        'descricao',
+        'usuario_solicitante_id',
+        'usuario_atendente_id',
+        'risco_id',
+        'status_id',
+        'setor_id',
+    ];
+
+    // Definir as relações
+    public function usuarioSolicitante()
+    {
+        return $this->belongsTo(User::class, 'usuario_solicitante_id');
+    }
+
+    public function usuarioAtendente()
+    {
+        return $this->belongsTo(User::class, 'usuario_atendente_id');
+    }
+
+    public function risco()
+    {
+        return $this->belongsTo(Risco::class, 'risco_id');
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
+
+    public function setor()
+    {
+        return $this->belongsTo(Setor::class, 'setor_id');
+    }
+}
